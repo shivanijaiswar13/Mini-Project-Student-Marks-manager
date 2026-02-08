25a85e85259bd01e8ac924c3ff34e9b9b647b7d4
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-02-08 19:55:13 +0530

    pass student

---
+++ script.js
@@ -0,0 +1,28 @@
+//Data (array)
+let marks = [45,78,60,32,90];
+
+// Print all marks(forEach)
+marks.forEach((mark)=>{
+    console.log(mark);
+});
+
+// Total marks (reduce)
+
+let total = marks.reduce((sum, mark)=>{
+    return sum+mark;
+},0)
+console.log("total marks", total);
+
+//Average marks
+let average = total/marks.length
+console.log("Average marks:", average);
+
+//Pass Student(filter)
+
+// Assume:pass marks = 40
+
+let passStudent = marks.filter((mark)=>{
+    return mark>40;
+});
+console.log("pass student marks:",passStudent);
+
